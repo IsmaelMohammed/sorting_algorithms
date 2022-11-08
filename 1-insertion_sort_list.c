1c90aac5653fe88c4f5f99ee451f57cc9462ebad
Ismael Mohammed <ismaelmohammed97@gmail.com>
2022-11-08 20:59:52 +0300

    Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,146 +1,56 @@
 #include "sort.h"
+
 /**
- * print_list_rev - Prints a doubly linkedd list of integers in reverse
- * @list: The list to be printed
+ * swap - function swaping two nodes
+ * @list: pointer to first item in a list
+ * @node1: pointer to node1
+ * @node2: pointer to node2
+ *
+ * Return: nothing
  */
-void print_list_rev(const listint_t *list, int i, int j)
-{
-	int n = 0;
-	const listint_t *tail;
 
-	while(list)
-	{
-		tail = list;
-		list = list->next;
-		n++;
-	}
-	printf("\x1b[33m%02d: \x1b[0m", i);
-	while (tail)
-	{
-		if (tail->next)
-			printf(" ");
-		if (n == i)
-			printf("\x1b[31m%02d\x1b[0m", tail->n);
-		else if (n == j)
-			printf("\x1b[32m%02d\x1b[0m", tail->n);
-		else
-			printf("%02d", tail->n);
-		tail = tail->prev;
-		n--;
-	}
-	printf("\n");
+void swap(listint_t **list, listint_t *node1, listint_t *node2)
+{
+        node1->next = node2->next;
+        if (node1->next)
+        {
+                node1->next->prev = node1;
+        }
+        node2->next = node1;
+        node2->prev = node1->prev;
+        node1->prev = node2;
+        if (node2->prev)
+        {
+                node2->prev->next = node2;
+        }
+        else
+        {
+                *list = node2;
+        }
 }
+
 /**
- * print_list2 - Prints a list of integers
- * @list: The list to be printed
+ * insertion_sort_list - sorting list by insertion algorithm
+ * @list: pointer to first item in a list
+ *
+ * Return: Nothing
  */
-void print_list2(const listint_t *list, int i, int j)
-{
-	int n = 0;
 
-	printf("\x1b[33m%02d: \x1b[0m", i);
-	while (list)
-	{
-		if (n > 0)
-			printf(" ");
-		if (n == i)
-			printf("\x1b[31m%02d\x1b[0m", list->n);
-		else if (n == j)
-			printf("\x1b[32m%02d\x1b[0m", list->n);
-		else
-			printf("%02d", list->n);
-		list = list->next;
-		n++;
-	}
-	printf("\n");
-}
-/**
- * printarr - Prints an array of integers
- * @list: The list to be printed
- */
-void printarr(int array[], size_t i, size_t j)
-{
-	size_t n = 0;
-
-	printf("\x1b[33m%02d: \x1b[0m", (int)i);
-	while (array[n])
-	{
-		if (n > 0)
-			printf(" ");
-		if (n == i)
-			printf("\x1b[31m%02d\x1b[0m", array[n]);
-		else if (n == j)
-			printf("\x1b[32m%02d\x1b[0m", array[n]);
-		else
-			printf("%02d", array[n]);
-		n++;
-	}
-	printf("\n");
-}
-
-void insertion_sort_arr(int array[], size_t size)
-{
-	size_t i, j, tmp;
-
-	i = j = 1;
-	for (i = 1; i < size; i++)
-	{
-		/* Main Algorithm */
-		for (j = i; j > 0 && array[j - 1] > array[j]; j--)
-		{
-			tmp = array[j];
-			array[j] = array[j - 1];
-			array[j - 1] = tmp;
-		}
-		printarr(array, i, j);
-	}
-}
-
-/**
-  * insertion_sort_list - insertion sort algorithm
-  * @list: Linked list to be sorted
-  */
 void insertion_sort_list(listint_t **list)
 {
-	int i, j;
-	listint_t *head, *walk, *swap;
+        listint_t *node;
 
-	i = j = 0;
-	head = *list;
-	/* Start at the second node */
-	head = head->next;
-	while (head->next)
-	{
-		walk = head;
-		j = i;
-		while (j > 0 && walk->prev && (walk->prev)->n > walk->n)
-		{
-			/*
-		printf("\x1b[35m%02d BEFORE: walk = %d, swap = %d, head = %d\n\x1b[0m", i, walk->n, swap->n, head->n);
-		*/
-			swap = walk;
-			walk = walk->prev;
-			if (walk->prev)
-				walk->prev->next = swap;
-			if (swap->next)
-				swap->next->prev = walk;
-			walk->next = swap->next;
-			swap->prev = walk->prev;
-			walk->prev = swap;
-			swap->next = walk;
-			if (swap->prev)
-				walk = swap->prev;
-			j--;
-			print_list2(*list, i, j);
-		/*
-		printf("\x1b[35m%02d AFTER: walk = %d, swap = %d, head = %d\n\x1b[0m", i, walk->n, swap->n, head->n);
-		*/
-		}
-		i++;
-		head = head->next;
-		print_list2(*list, i, j);
-		/*
-		print_list_rev(*list, i, j);
-		*/
-	}
+        if (!list || !*list || !(*list)->next)
+                return;
+
+        node = (*list)->next;
+        while (node)
+        {
+                while (node->prev && node->n < node->prev->n)
+                {
+                        swap(list, node->prev, node);
+                        print_list(*list);
+                }
+                node = node->next;
+        }
 }
